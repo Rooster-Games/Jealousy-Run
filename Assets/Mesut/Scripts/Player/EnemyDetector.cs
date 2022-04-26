a49122817f018cfffac60e8670d81a22f3f43644
Mesut Engin <mesutengin@roostergames.net>
2022-04-26 18:31:50 +0300

    v0.0.22 - tokat animasyonlari test ediliyor

--- Assets/Mesut/Scripts/Player/EnemyDetector.cs
+++ Assets/Mesut/Scripts/Player/EnemyDetector.cs
@@ -15,19 +15,26 @@ namespace JR
 
         private void OnTriggerEnter(Collider other)
         {
+            Debug.Log("EnemyDetector - OnTriggerEnter");
             var otherGenderInfo = other.GetComponent<GenderInfo>();
             if(otherGenderInfo == null)
             {
+                Debug.Log(other.gameObject.name);
                 Debug.Log("GenderInfoBulunamadi");
                 return;
             }
+
+            Debug.Log("Other Gender: " + otherGenderInfo.Gender);
+            Debug.Log("My Gender:" + _singleController.GenderInfo.Gender);
             if(otherGenderInfo.Gender == _singleController.GenderInfo.Gender)
             {
                 if (other.isTrigger)
                     other.enabled = false;
 
+                Debug.Log("BeforeSlap");
                 _singleController.Slap();
             }
+            Debug.Log("Ending Of OnTriggerEnter");
         }
 
         public class InitParameters
